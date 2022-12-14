b36e6280618b259c94d8dc6f504100f4c3968cf2
Andreas Motl <andreas.motl@cicerops.de>
2022-12-14 20:53:59 +0100

    Format code using `black` and `isort`

--- pygource.py
+++ pygource.py
@@ -15,17 +15,19 @@ Synopsis::
         # --overwrite
 """
 import os
-import sys
 import re
-import shutil
 import shlex
+import shutil
 import subprocess
+import sys
 
 
 class GourceRenderer(object):
     """Renders project history using 'gource'."""
 
-    def __init__(self, source_path, target_path, overwrite = False, audio_source = None, audio_loops = None, time_lapse = False):
+    def __init__(
+        self, source_path, target_path, overwrite=False, audio_source=None, audio_loops=None, time_lapse=False
+    ):
 
         self.project_path = source_path
         self.output_path = target_path
@@ -58,13 +60,13 @@ class GourceRenderer(object):
         self.time_lapse = time_lapse
 
     def get_gource_command(self, path: str, title: str):
-        speed_options = '--seconds-per-day 5 --time-scale 1.5'
+        speed_options = "--seconds-per-day 5 --time-scale 1.5"
         if self.time_lapse:
-            speed_options = '--seconds-per-day 1 --time-scale 4'
+            speed_options = "--seconds-per-day 1 --time-scale 4"
         gource_options = {
-            'path': path,
-            'title': title,
-            'speed_options': speed_options,
+            "path": path,
+            "title": title,
+            "speed_options": speed_options,
         }
         command = self.gource_cmd_tpl % gource_options
         return command
@@ -73,8 +75,8 @@ class GourceRenderer(object):
         # TODO: enhance song picker (e.g. random or mapped selection from a directory)
         if self.audio_source and os.path.isfile(self.audio_source):
             return self.audio_source
-        elif os.path.isfile(os.environ.get('GOURCE_AUDIO_SOURCE', '')):
-            return os.environ.get('GOURCE_AUDIO_SOURCE')
+        elif os.path.isfile(os.environ.get("GOURCE_AUDIO_SOURCE", "")):
+            return os.environ.get("GOURCE_AUDIO_SOURCE")
 
     def process_project(self, path: str, name: str):
 
@@ -88,7 +90,7 @@ class GourceRenderer(object):
             return False
 
         mi = MediaInfo(video_file)
-        if 'video' in mi.get_streams():
+        if "video" in mi.get_streams():
             print("INFO: Video:    ", video_file)
             print("INFO: Duration: ", mi.duration)
 
@@ -114,7 +116,7 @@ class GourceRenderer(object):
 
         print("-" * 42)
         print("Creating video '%s'" % vr.get_video_file())
-        cmd = self.get_gource_command(path = project_path, title = video_filename) + ' | ' + vr.get_command()
+        cmd = self.get_gource_command(path=project_path, title=video_filename) + " | " + vr.get_command()
         print("command:", cmd)
         print("-" * 42)
 
@@ -135,7 +137,7 @@ class GourceRenderer(object):
             print("ERROR while executing command '%s'" % command)
             return False
 
-    def loop_audio(self, audio_source, times = 2):
+    def loop_audio(self, audio_source, times=2):
 
         # TODO: remove looped audio file after usage
 
@@ -148,7 +150,7 @@ class GourceRenderer(object):
             return mp3wrap_file
 
         # WTF?
-        mp3wrap_file_real = mp3wrap_file.replace('.mp3', '_MP3WRAP.mp3')
+        mp3wrap_file_real = mp3wrap_file.replace(".mp3", "_MP3WRAP.mp3")
         if os.path.exists(mp3wrap_file_real):
             os.unlink(mp3wrap_file_real)
 
@@ -160,8 +162,8 @@ class GourceRenderer(object):
 class MediaInfo(object):
 
     """
-      Duration: 00:00:01.73, start: 0.000000, bitrate: 662 kb/s
-        Stream #0.0(und): Video: h264, yuv420p, 1024x768, 651 kb/s, 60 fps, 59.94 tbr, 60 tbn, 120 tbc
+    Duration: 00:00:01.73, start: 0.000000, bitrate: 662 kb/s
+      Stream #0.0(und): Video: h264, yuv420p, 1024x768, 651 kb/s, 60 fps, 59.94 tbr, 60 tbn, 120 tbc
     """
 
     def __init__(self, mediafile):
@@ -176,17 +178,19 @@ class MediaInfo(object):
         cmd = "ffprobe -i '%s'" % self.mediafile
         print("MediaInfo ffmpeg command:", cmd)
         output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT).decode("utf-8")
-        #print "MediaInfo output:", output
+        # print("MediaInfo output:", output)
         self.raw = output
 
     def parse_info(self):
-        r_duration = re.compile('.*Duration: ([.:0-9]+)')
-        r_stream = re.compile('.*Stream .*: (Video|Audio)')
+        r_duration = re.compile(".*Duration: ([.:0-9]+)")
+        r_stream = re.compile(".*Stream .*: (Video|Audio)")
         for line in self.raw.split("\n"):
             m = r_duration.match(line)
-            if m: self.duration = m.group(1)
+            if m:
+                self.duration = m.group(1)
             m = r_stream.match(line)
-            if m: self.streams.append(m.group(1).lower())
+            if m:
+                self.streams.append(m.group(1).lower())
 
     def get_duration(self):
         return self.duration
@@ -196,7 +200,6 @@ class MediaInfo(object):
 
 
 class VideoRecorder(object):
-
     def __init__(self, video_path, video_name):
         self.video_path = video_path
         self.video_name = video_name
@@ -208,27 +211,29 @@ class VideoRecorder(object):
         # Some remarks about "ffmpeg" options:
         #   - The encoder 'aac' is experimental but experimental codecs are not enabled,
         #     add '-strict -2' if you want to use it.
-        command = f"""
+        command = (
+            f"""
             ffmpeg -y \\
                 -r 60 \\
                 -vcodec ppm -f image2pipe -i - \\
                 -vcodec libx264 -pix_fmt yuv420p -preset medium -threads 0 -strict -2 \\
                 "{self.video_file}" \\
-            """ % arguments
+            """
+            % arguments
+        )
         return command
 
     def get_video_file(self):
         return os.path.join(self.video_path, self.video_name + self.get_extension())
 
     def get_extension(self):
-        return '.mp4'
+        return ".mp4"
 
     def exists(self):
         return os.path.exists(self.get_video_file())
 
 
 class VideoAudioMixer(object):
-
     def __init__(self, video_file, audio_file):
         self.video_file = video_file
         self.audio_file = audio_file
@@ -236,11 +241,15 @@ class VideoAudioMixer(object):
 
     def run(self):
         extension = os.path.splitext(self.video_file)[1]
-        tmpfile = self.video_file + '.tmp' + extension
-        cmd = "ffmpeg -y -i '%s' -i '%s' -vcodec copy -acodec copy -async 1 -shortest '%s'" % (self.video_file, self.audio_file, tmpfile)
+        tmpfile = self.video_file + ".tmp" + extension
+        cmd = "ffmpeg -y -i '%s' -i '%s' -vcodec copy -acodec copy -async 1 -shortest '%s'" % (
+            self.video_file,
+            self.audio_file,
+            tmpfile,
+        )
         print("VideoAudioMixer ffmpeg command:", cmd)
         output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
-        #print "VideoAudioMixer ffmpeg output:", output
+        # print "VideoAudioMixer ffmpeg output:", output
         shutil.move(tmpfile, self.video_file)
 
 
@@ -257,14 +266,23 @@ def render_single():
     """
 
     from optparse import OptionParser
+
     parser = OptionParser()
-    parser.add_option("-p", "--path", dest = "path", help = "path to vcs repository")
-    parser.add_option("-o", "--outdir", dest = "outdir", help = "path to output directory")
-    parser.add_option("-n", "--name", dest = "name", help = "project name (output video basename w/o extension) [optional]")
-    parser.add_option("-a", "--audio-source", dest = "audio_source", type = "str", help = "path to background song")
-    parser.add_option("-l", "--audio-loops", dest = "audio_loops", type = "int", help = "how often to loop the given background song (*must* be longer than video since ffmpeg is started with option '-shortest')")
-    parser.add_option("-O", "--overwrite", dest = "overwrite", action = "store_true", help = "whether to overwrite video files")
-    parser.add_option("-t", "--time-lapse", dest = "time_lapse", action = "store_true", help = "run in time-lapse mode")
+    parser.add_option("-p", "--path", dest="path", help="path to vcs repository")
+    parser.add_option("-o", "--outdir", dest="outdir", help="path to output directory")
+    parser.add_option("-n", "--name", dest="name", help="project name (output video basename w/o extension) [optional]")
+    parser.add_option("-a", "--audio-source", dest="audio_source", type="str", help="path to background song")
+    parser.add_option(
+        "-l",
+        "--audio-loops",
+        dest="audio_loops",
+        type="int",
+        help="how often to loop the given background song (*must* be longer than video since ffmpeg is started with option '-shortest')",
+    )
+    parser.add_option(
+        "-O", "--overwrite", dest="overwrite", action="store_true", help="whether to overwrite video files"
+    )
+    parser.add_option("-t", "--time-lapse", dest="time_lapse", action="store_true", help="run in time-lapse mode")
     (options, args) = parser.parse_args()
 
     # Sanity checks.
@@ -287,9 +305,16 @@ def render_single():
     print("Rendering project history of single project '%s <%s>' using 'gource'" % (options.name, options.path))
     source_path = options.path
     target_path = options.outdir
-    gr = GourceRenderer(source_path, target_path, overwrite = options.overwrite, audio_source = options.audio_source, audio_loops = options.audio_loops, time_lapse = options.time_lapse)
+    gr = GourceRenderer(
+        source_path,
+        target_path,
+        overwrite=options.overwrite,
+        audio_source=options.audio_source,
+        audio_loops=options.audio_loops,
+        time_lapse=options.time_lapse,
+    )
     gr.process_project(path=options.path, name=options.name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     render_single()
